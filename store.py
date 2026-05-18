5537d1e79f1f66d90a480861bca56cdad859b21c
prabhust <stprabhu6@gmail.com>
2026-05-17 23:32:51 -0400

    Add store.py

---
+++ store.py
@@ -0,0 +1,11 @@
+# Variables and Data Types
+item_name = "Coffee Maker"  # String
+price = 100                 # Int
+is_on_sale = True           # Bool
+
+# Basic Math & Conditionals (If-Else)
+if is_on_sale:
+    discounted_price = price - 20
+    print(f"The {item_name} is on sale for ${discounted_price}!")
+else:
+    print(f"The {item_name} is normal price: ${price}.")
